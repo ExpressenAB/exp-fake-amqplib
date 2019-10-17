af0db41d547a5b978629bd730d16fb96a30286e9
Mattias Norlander (EXT Exp) <mattias.norlander@bnmc02vd2ufhv2q.ad.bonniernews.se>
2019-10-17 16:35:49 +0200

    Don't invoke pub callback unless confirm channel

--- index.js
+++ index.js
@@ -12,80 +12,83 @@ function connect(url, options, connCallback) {
   }
 
   const connection = {
-    createChannel,
-    createConfirmChannel: createChannel,
+    createChannel: createChannel(false),
+    createConfirmChannel: createChannel(true),
     on: function () {},
     close: resetMock,
   };
 
   return connCallback(null, connection);
 
-  function createChannel(channelCallback) {
-    channelCallback(null, {
-      assertQueue,
-      assertExchange,
-      bindQueue,
-      publish,
-      consume,
-      deleteQueue,
-      ack,
-      nack,
-      prefetch,
-      on,
-    });
+  function createChannel(confirm) {
+    return (channelCallback) => {
+      channelCallback(null, {
+        assertQueue,
+        assertExchange,
+        bindQueue,
+        publish,
+        consume,
+        deleteQueue,
+        ack,
+        nack,
+        prefetch,
+        on,
+      });
 
-    function assertQueue(queue, qOptions, qCallback) {
-      qCallback = qCallback || function () {};
-      setIfUndef(queues, queue, {messages: [], subscribers: [], options: qOptions});
-      qCallback();
-    }
-
-    function assertExchange(exchange, type, exchOptions, exchCallback) {
-      if (typeof (exchOptions) === "function") {
-        exchCallback = exchOptions;
-        exchOptions = {};
+      function assertQueue(queue, qOptions, qCallback) {
+        qCallback = qCallback || function () {};
+        setIfUndef(queues, queue, {messages: [], subscribers: [], options: qOptions});
+        qCallback();
       }
-      setIfUndef(exchanges, exchange, {bindings: [], options: exchOptions, type: type});
-      return exchCallback && exchCallback();
-    }
 
-    function bindQueue(queue, exchange, key, args, bindCallback) {
-      bindCallback = bindCallback || function () {};
-      if (!exchanges[exchange]) return bindCallback("Bind to non-existing exchange " + exchange);
-      const re = "^" + key.replace(".", "\\.").replace("#", "(\\w|\\.)+").replace("*", "\\w+") + "$";
-      exchanges[exchange].bindings.push({regex: new RegExp(re), queueName: queue});
-      bindCallback();
-    }
+      function assertExchange(exchange, type, exchOptions, exchCallback) {
+        if (typeof (exchOptions) === "function") {
+          exchCallback = exchOptions;
+          exchOptions = {};
+        }
+        setIfUndef(exchanges, exchange, {bindings: [], options: exchOptions, type: type});
+        return exchCallback && exchCallback();
+      }
 
-    function publish(exchange, routingKey, content, props, pubCallback) {
-      pubCallback = pubCallback || function () {};
-      if (!exchanges[exchange]) return pubCallback("Publish to non-existing exchange " + exchange);
-      const bindings = exchanges[exchange].bindings;
-      const matchingBindings = bindings.filter((b) => b.regex.test(routingKey));
-      matchingBindings.forEach((binding) => {
-        const subscribers = queues[binding.queueName] ? queues[binding.queueName].subscribers : [];
-        subscribers.forEach((sub) => {
-          const message = {fields: {routingKey: routingKey}, properties: props, content: content};
-          sub(message);
+      function bindQueue(queue, exchange, key, args, bindCallback) {
+        bindCallback = bindCallback || function () {};
+        if (!exchanges[exchange]) return bindCallback("Bind to non-existing exchange " + exchange);
+        const re = "^" + key.replace(".", "\\.").replace("#", "(\\w|\\.)+").replace("*", "\\w+") + "$";
+        exchanges[exchange].bindings.push({regex: new RegExp(re), queueName: queue});
+        bindCallback();
+      }
+
+      function publish(exchange, routingKey, content, props, pubCallback) {
+        pubCallback = pubCallback || function () {};
+        if (!exchanges[exchange]) return pubCallback("Publish to non-existing exchange " + exchange);
+        const bindings = exchanges[exchange].bindings;
+        const matchingBindings = bindings.filter((b) => b.regex.test(routingKey));
+        matchingBindings.forEach((binding) => {
+          const subscribers = queues[binding.queueName] ? queues[binding.queueName].subscribers : [];
+          subscribers.forEach((sub) => {
+            const message = {fields: {routingKey: routingKey}, properties: props, content: content};
+            sub(message);
+          });
         });
-      });
-      return pubCallback && pubCallback();
-    }
+        if (confirm) pubCallback();
+        return true;
+      }
 
-    function consume(queue, handler) {
-      queues[queue].subscribers.push(handler);
-    }
+      function consume(queue, handler) {
+        queues[queue].subscribers.push(handler);
+      }
 
-    function deleteQueue(queue) {
-      setImmediate(() => {
-        delete queues[queue];
-      });
-    }
+      function deleteQueue(queue) {
+        setImmediate(() => {
+          delete queues[queue];
+        });
+      }
 
-    function ack() {}
-    function nack() {}
-    function prefetch() {}
-    function on() {}
+      function ack() {}
+      function nack() {}
+      function prefetch() {}
+      function on() {}
+    };
   }
 }
 
